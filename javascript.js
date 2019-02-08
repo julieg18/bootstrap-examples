66baba626676283633099cade5919eb01c5d4752
julieg18 <julieheartcrochet@gmail.com>
2019-02-08 08:09:36 -0600

    Finished javascript functions

--- javascript.js
+++ javascript.js
@@ -2,22 +2,36 @@ const fragments = {
   r1D1: '<p>This sentence is made with a <code>&lt;p&gt;</code> tag but I can make it look like a <code>&lt;h5&gt;</code> tag by adding a "h5" class.</p>',
   r1D2: '',
   r1D3: '',
-  r1D4: '',
   r2D1: '',
   r2D2: '',
   r2D3: '',
-  r2D4: ''
+  r3D1: '',
+  r3D2: '',
+  r3D3: '',
+  r4D1: '',
+  r4D2: '',
+  r4D3: '',
+  r5D1: '',
+  r5D2: '',
+  r5D3: ''
 };
 
 const originalFragments = {
   r1D1: '<p class="h5">This sentence is made with a <code>&lt;p&gt;</code> tag but I can make it look like a <code>&lt;h5&gt;</code> tag by adding a "h5" class.</p>',
   r1D2: '',
   r1D3: '',
-  r1D4: '',
   r2D1: '',
   r2D2: '',
   r2D3: '',
-  r2D4: ''
+  r3D1: '',
+  r3D2: '',
+  r3D3: '',
+  r4D1: '',
+  r4D2: '',
+  r4D3: '',
+  r5D1: '',
+  r5D2: '',
+  r5D3: ''
 };
 
 function placeFragmentsOnR1D1() {
@@ -42,6 +56,109 @@ function placeFragmentsOnR1D3() {
 
 function placeOriginalFragmentsOnR1D3() {
   document.getElementById("r1d3-example").innerHTML = originalFragments.r1D3;
+}; 
+
+function placeFragmentsOnR2D1() {
+  document.getElementById("r2d1-example").innerHTML = fragments.r2D1;
 };
+
+function placeOriginalFragmentsOnR2D1() {
+  document.getElementById("r2d1-example").innerHTML = originalFragments.r2D1;
+};
+
+function placeFragmentsOnR2D2() {
+  document.getElementById("r2d2-example").innerHTML = fragments.r2D2
+}
+
+function placeOriginalFragmentsOnR2D2() {
+  document.getElementById("r2d2-example").innerHTML = originalFragments.r2D2
+}
+
+function placeFragmentsOnR2D3() {
+  document.getElementById("r1d3-example").innerHTML = fragments.r2D3;
+};
+
+function placeOriginalFragmentsOnR2D3() {
+  document.getElementById("r2d3-example").innerHTML = originalFragments.r2D3;
+};
+
+function placeFragmentsOnR3D1() {
+  document.getElementById("r3d1-example").innerHTML = fragments.r3D1;
+};
+
+function placeOriginalFragmentsOnR3D1() {
+  document.getElementById("r3d1-example").innerHTML = originalFragments.r3D1;
+};
+
+function placeFragmentsOnR3D2() {
+  document.getElementById("r3d2-example").innerHTML = fragments.r3D2;
+};
+
+function placeOriginalFragmentsOnR3D2() {
+  document.getElementById("r3d2-example").innerHTML = originalFragments.r3D2;
+};
+
+function placeFragmentsOnR3D3() {
+  document.getElementById("r3d3-example").innerHTML = fragments.r3D3;
+};
+
+function placeOriginalFragmentsOnR3D3() {
+  document.getElementById("r3d3-example").innerHTML = originalFragments.r3D3;
+};
+
+function placeFragmentsOnR4D1() {
+  document.getElementById("r4d1-example").innerHTML = fragments.r4D1;
+};
+
+function placeOriginalFragmentsOnR4D1() {
+  document.getElementById("r4d1-example").innerHTML = originalFragments.r4D1;
+};
+
+function placeFragmentsOnR4D2() {
+  document.getElementById("r4d2-example").innerHTML = fragments.r4D2;
+};
+
+function placeOriginalFragmentsOnR4D2() {
+  document.getElementById("r4d2-example").innerHTML = originalFragments.r4D2;
+};
+
+function placeFragmentsOnR4D3() {
+  document.getElementById("r4d3-example").innerHTML = fragments.r4D3;
+};
+
+function placeOriginalFragmentsOnR4D3() {
+  document.getElementById("r4d3-example").innerHTML = originalFragments.r4D3;
+};
+
+function placeFragmentsOnR5D1() {
+  document.getElementById("r5d1-example").innerHTML = fragments.r5D1;
+};
+
+function placeOriginalFragmentsOnR5D1() {
+  document.getElementById("r5d1-example").innerHTML = originalFragments.r5D1;
+};
+
+function placeFragmentsOnR5D2() {
+  document.getElementById("r5d2-example").innerHTML = fragments.r5D2;
+};
+
+function placeOriginalFragmentsOnR5D2() {
+  document.getElementById("r5d2-example").innerHTML = originalFragments.r5D2;
+};
+
+function placeFragmentsOnR5D1() {
+  document.getElementById("r5d1-example").innerHTML = fragments.r5D1;
+};
+
+function placeOriginalFragmentsOnR5D3() {
+  document.getElementById("r5d3-example").innerHTML = originalFragments.r5D3;
+};
+
+
+
+
+
+
+
 
 
