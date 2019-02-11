9fb8d5ffe39bc3b92b30ecf7c4cc20881eccef2e
julieg18 <julieheartcrochet@gmail.com>
2019-02-11 06:50:39 -0600

    edited javascript functions

--- javascript.js
+++ javascript.js
@@ -35,123 +35,123 @@ const originalFragments = {
 };
 
 function placeFragmentsOnR1D1() {
-  document.getElementById("r1d1-example").innerHTML = fragments.r1D1;
+  return document.getElementById("r1d1-example").innerHTML = fragments.r1D1;
 };
 
 function placeOriginalFragmentsOnR1D1() {
-  document.getElementById("r1d1-example").innerHTML = originalFragments.r1D1;
+  return document.getElementById("r1d1-example").innerHTML = originalFragments.r1D1;
 };
 
 function placeFragmentsOnR1D2() {
-  document.getElementById("r1d2-example").innerHTML = fragments.r1D2;
+  return document.getElementById("r1d2-example").innerHTML = fragments.r1D2;
 };
 
 function placeOriginalFragmentsOnR1D2() {
-  document.getElementById("r1d2-example").innerHTML = originalFragments.r1D2;
+  return document.getElementById("r1d2-example").innerHTML = originalFragments.r1D2;
 };
 
 function placeFragmentsOnR1D3() {
-  document.getElementById("r1d3-example").innerHTML = fragments.r1D3;
+  return document.getElementById("r1d3-example").innerHTML = fragments.r1D3;
 };
 
 function placeOriginalFragmentsOnR1D3() {
-  document.getElementById("r1d3-example").innerHTML = originalFragments.r1D3;
+  return document.getElementById("r1d3-example").innerHTML = originalFragments.r1D3;
 }; 
 
 function placeFragmentsOnR2D1() {
-  document.getElementById("r2d1-example").innerHTML = fragments.r2D1;
+  return document.getElementById("r2d1-example").innerHTML = fragments.r2D1;
 };
 
 function placeOriginalFragmentsOnR2D1() {
-  document.getElementById("r2d1-example").innerHTML = originalFragments.r2D1;
+  return document.getElementById("r2d1-example").innerHTML = originalFragments.r2D1;
 };
 
 function placeFragmentsOnR2D2() {
-  document.getElementById("r2d2-example").innerHTML = fragments.r2D2
+  return document.getElementById("r2d2-example").innerHTML = fragments.r2D2
 }
 
 function placeOriginalFragmentsOnR2D2() {
-  document.getElementById("r2d2-example").innerHTML = originalFragments.r2D2
+  return document.getElementById("r2d2-example").innerHTML = originalFragments.r2D2
 }
 
 function placeFragmentsOnR2D3() {
-  document.getElementById("r2d3-example").innerHTML = fragments.r2D3;
+  return document.getElementById("r2d3-example").innerHTML = fragments.r2D3;
 };
 
 function placeOriginalFragmentsOnR2D3() {
-  document.getElementById("r2d3-example").innerHTML = originalFragments.r2D3;
+  return document.getElementById("r2d3-example").innerHTML = originalFragments.r2D3;
 };
 
 function placeFragmentsOnR3D1() {
-  document.getElementById("r3d1-example").innerHTML = fragments.r3D1;
+  return document.getElementById("r3d1-example").innerHTML = fragments.r3D1;
 };
 
 function placeOriginalFragmentsOnR3D1() {
-  document.getElementById("r3d1-example").innerHTML = originalFragments.r3D1;
+  return document.getElementById("r3d1-example").innerHTML = originalFragments.r3D1;
 };
 
 function placeFragmentsOnR3D2() {
-  document.getElementById("r3d2-example").innerHTML = fragments.r3D2;
+  return document.getElementById("r3d2-example").innerHTML = fragments.r3D2;
 };
 
 function placeOriginalFragmentsOnR3D2() {
-  document.getElementById("r3d2-example").innerHTML = originalFragments.r3D2;
+  return document.getElementById("r3d2-example").innerHTML = originalFragments.r3D2;
 };
 
 function placeFragmentsOnR3D3() {
-  document.getElementById("r3d3-example").innerHTML = fragments.r3D3;
+  return document.getElementById("r3d3-example").innerHTML = fragments.r3D3;
 };
 
 function placeOriginalFragmentsOnR3D3() {
-  document.getElementById("r3d3-example").innerHTML = originalFragments.r3D3;
+  return document.getElementById("r3d3-example").innerHTML = originalFragments.r3D3;
 };
 
 function placeFragmentsOnR4D1() {
-  document.getElementById("r4d1-example").innerHTML = fragments.r4D1;
+  return document.getElementById("r4d1-example").innerHTML = fragments.r4D1;
 };
 
 function placeOriginalFragmentsOnR4D1() {
-  document.getElementById("r4d1-example").innerHTML = originalFragments.r4D1;
+  return document.getElementById("r4d1-example").innerHTML = originalFragments.r4D1;
 };
 
 function placeFragmentsOnR4D2() {
-  document.getElementById("r4d2-example").innerHTML = fragments.r4D2;
+  return document.getElementById("r4d2-example").innerHTML = fragments.r4D2;
 };
 
 function placeOriginalFragmentsOnR4D2() {
-  document.getElementById("r4d2-example").innerHTML = originalFragments.r4D2;
+  return document.getElementById("r4d2-example").innerHTML = originalFragments.r4D2;
 };
 
 function placeFragmentsOnR4D3() {
-  document.getElementById("r4d3-example").innerHTML = fragments.r4D3;
+  return document.getElementById("r4d3-example").innerHTML = fragments.r4D3;
 };
 
 function placeOriginalFragmentsOnR4D3() {
-  document.getElementById("r4d3-example").innerHTML = originalFragments.r4D3;
+  return document.getElementById("r4d3-example").innerHTML = originalFragments.r4D3;
 };
 
 function placeFragmentsOnR5D1() {
-  document.getElementById("r5d1-example").innerHTML = fragments.r5D1;
+  return document.getElementById("r5d1-example").innerHTML = fragments.r5D1;
 };
 
 function placeOriginalFragmentsOnR5D1() {
-  document.getElementById("r5d1-example").innerHTML = originalFragments.r5D1;
+  return document.getElementById("r5d1-example").innerHTML = originalFragments.r5D1;
 };
 
 function placeFragmentsOnR5D2() {
-  document.getElementById("r5d2-example").innerHTML = fragments.r5D2;
+  return document.getElementById("r5d2-example").innerHTML = fragments.r5D2;
 };
 
 function placeOriginalFragmentsOnR5D2() {
-  document.getElementById("r5d2-example").innerHTML = originalFragments.r5D2;
+  return document.getElementById("r5d2-example").innerHTML = originalFragments.r5D2;
 };
 
 function placeFragmentsOnR5D3() {
-  document.getElementById("r5d3-example").innerHTML = fragments.r5D3;
+  return document.getElementById("r5d3-example").innerHTML = fragments.r5D3;
 };
 
 function placeOriginalFragmentsOnR5D3() {
-  document.getElementById("r5d3-example").innerHTML = originalFragments.r5D3;
+  return document.getElementById("r5d3-example").innerHTML = originalFragments.r5D3;
 };
 
 
